b4992ccc088c3426f84f5c2f8bb950030a7c72ec
Nzablue <nazareyesbaccaro@gmail.com>
2025-11-07 11:51:33 -0300

    se quitaron js y css innecesarios

--- assets/Backend/script.js
+++
@@ -1,6 +0,0 @@
-const btngame = document.querySelector('.btn-game')
-    const navbar = document.querySelector('.navbar')
-
-    btngame.addEventListener('click', () => {
-        navbar.classList.toggle('active')
-    });
